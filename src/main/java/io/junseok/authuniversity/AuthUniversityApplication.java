0158b034facabbb8ec598bb305686c884081896e
JunRock <wnstjr120422@naver.com>
2024-03-11 17:07:09 +0900

    delete : main

--- src/main/java/io/junseok/authuniversity/AuthUniversityApplication.java
+++ src/main/java/io/junseok/authuniversity/AuthUniversityApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthUniversityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthUniversityApplication.class, args);
+		SpringApplication.run(AuthUniversityApplication.class,  args);
 	}
 }
