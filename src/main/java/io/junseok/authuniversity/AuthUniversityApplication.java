5039035e5f8ef40842279d75f35bc7b44138b802
JunRock <wnstjr120422@naver.com>
2024-06-03 00:21:35 +0900

    fix : main 클래스 수정

--- src/main/java/io/junseok/authuniversity/AuthUniversityApplication.java
+++ src/main/java/io/junseok/authuniversity/AuthUniversityApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/*
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
@@ -15,3 +16,4 @@ public class AuthUniversityApplication {
 		SpringApplication.run(AuthUniversityApplication.class, args);
 	}
 }
+*/
